6386f6c89ddc628e964056e6fb025a338f517876
Aboodiii <89215713+Aboodiii@users.noreply.github.com>
2024-08-07 16:55:02 +0300

    Update supplier.js

--- Back-end/Routes/supplier.js
+++ Back-end/Routes/supplier.js
@@ -4,7 +4,7 @@ const router=express.Router()
 const {getAllSuppliers,getSupplierById,createSupplier, UpdateSupplier,deleteSupplier}=require('../Controllers/supplier')
 
 router.get('/getAllSuppliers',getAllSuppliers)
-router.get('/getSupplierById:id',getSupplierById)
+router.get('/getSupplierById/:id',getSupplierById)
 router.post('/createSupplier',createSupplier)
 router.put('/updateSupplier/:id',UpdateSupplier)
 router.delete('/deleteSupplier/:id',deleteSupplier)
