80dcbd340e8d8599e0753f738709f5dcc7337b63
unknown <naodtadele1@gmail.com>
2024-08-07 19:00:45 +0300

    warehouse added to api

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const items=require('./Routes/items')
 const brand=require('./Routes/brand')
+const warehouse=require('./Routes/warehouse')
 const supplier=require('./Routes/supplier')
 const app=express();
 const connectDB=require('./db/connect')
@@ -18,7 +19,7 @@ require('dotenv').config()
 
 app.use('/api/brands',brand)
 app.use('/api/supplier',supplier)
-
+app.use('/api/warehouse',warehouse)
 
 
 
