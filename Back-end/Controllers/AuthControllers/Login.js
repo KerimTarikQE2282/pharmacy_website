7c263c42374c13cd66eb58761fa844acd4a3097b
KerimTarikQE2282 <git config --global user.email you@example.com>
2024-08-10 07:00:39 +0300

    Login and registration almost done

---
+++ Back-end/Controllers/AuthControllers/Login.js
@@ -0,0 +1,21 @@
+const StatusCodes=require('http-status-codes')
+const User=require('../../models/User');
+const { UnauthenticatedError } = require('../../errors');
+
+
+const login=async (req,res)=>{
+const {email,password}=req.body;
+if(!email || !password){
+    throw new BadRequestError("please provide email and password")
+}
+const userAccount=await User.findOne({email:email})
+if(!userAccount){
+    throw new UnauthenticatedError("User Doesnt Exist");
+}
+const isPasswordCorrect=await userAccount.passwordChecker(password);
+if(!isPasswordCorrect){
+    throw new UnauthenticatedError("Invalid Credentials")
+}
+res.status(StatusCodes.OK).json({user:userAccount.username,token:userAccount.createJWT()});
+}
+module.exports={login}
